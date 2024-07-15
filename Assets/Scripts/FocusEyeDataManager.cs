246613ef69b724b8ce5d387ac48747572ad0a37d
Denny0097 <denny890907@gmail.com>
2024-07-15 17:51:47 +0800

    Modified FocusEyeDataManager.cs for allowing eye tracking storage. Modified item positions.

--- Assets/Scripts/FocusEyeDataManager.cs
+++ Assets/Scripts/FocusEyeDataManager.cs
@@ -8,27 +8,27 @@ using Wave.XR;
 public class FocusEyeData
 {
     //Combined Eye:
-    public Vector3 CombinedEyeOrigin;
+    //public Vector3 CombinedEyeOrigin;
     public Vector3 CombindedEyeDirectionNormalized;
 
-    //Left Eye:
-    //
-    public Vector3 LeftEyeOrigin;
-    //
-    public Vector3 LeftEyeDirectionNormalized;
-    //睜眼閉眼
-    public float LeftEyeOpenness;
-    //瞳孔直徑
-    public float LeftEyePupilDiameter;
-    //瞳孔位置
-    public Vector2 LeftEyePupilPositionInSensorArea;
+    ////Left Eye:
+    ////
+    //public Vector3 LeftEyeOrigin;
+    ////
+    //public Vector3 LeftEyeDirectionNormalized;
+    ////睜眼閉眼
+    //public float LeftEyeOpenness;
+    ////瞳孔直徑
+    //public float LeftEyePupilDiameter;
+    ////瞳孔位置
+    //public Vector2 LeftEyePupilPositionInSensorArea;
 
-    //Right Eye:
-    public Vector3 RightEyeOrigin;
-    public Vector3 RightEyeDirectionNormalized;
-    public float RightEyeOpenness;
-    public float RightEyePupilDiameter;
-    public Vector2 RightEyePupilPositionInSensorArea;
+    ////Right Eye:
+    //public Vector3 RightEyeOrigin;
+    //public Vector3 RightEyeDirectionNormalized;
+    //public float RightEyeOpenness;
+    //public float RightEyePupilDiameter;
+    //public Vector2 RightEyePupilPositionInSensorArea;
 }
 
 public class FocusEyeDataManager : MonoBehaviour
@@ -39,22 +39,23 @@ public class FocusEyeDataManager : MonoBehaviour
         bool result = true;
         FocusEyeData data = new FocusEyeData();
 
-        result &= EyeManager.Instance.GetCombinedEyeOrigin(out data.CombinedEyeOrigin);
+        //result &= EyeManager.Instance.GetCombinedEyeOrigin(out data.CombinedEyeOrigin);
         result &= EyeManager.Instance.GetCombindedEyeDirectionNormalized(out data.CombindedEyeDirectionNormalized);
-        result &= EyeManager.Instance.GetLeftEyeOrigin(out data.LeftEyeOrigin);
-        result &= EyeManager.Instance.GetLeftEyeDirectionNormalized(out data.LeftEyeDirectionNormalized);
-        result &= EyeManager.Instance.GetLeftEyeOpenness(out data.LeftEyeOpenness);
-        result &= EyeManager.Instance.GetLeftEyePupilDiameter(out data.LeftEyePupilDiameter);
-        result &= EyeManager.Instance.GetLeftEyePupilPositionInSensorArea(out data.LeftEyePupilPositionInSensorArea);
-        result &= EyeManager.Instance.GetRightEyeOrigin(out data.RightEyeOrigin);
-        result &= EyeManager.Instance.GetRightEyeDirectionNormalized(out data.RightEyeDirectionNormalized);
-        result &= EyeManager.Instance.GetRightEyeOpenness(out data.RightEyeOpenness);
-        result &= EyeManager.Instance.GetRightEyePupilDiameter(out data.RightEyePupilDiameter);
-        result &= EyeManager.Instance.GetRightEyePupilPositionInSensorArea(out data.RightEyePupilPositionInSensorArea);
-        
+        //result &= EyeManager.Instance.GetLeftEyeOrigin(out data.LeftEyeOrigin);
+        //result &= EyeManager.Instance.GetLeftEyeDirectionNormalized(out data.LeftEyeDirectionNormalized);
+        //result &= EyeManager.Instance.GetLeftEyeOpenness(out data.LeftEyeOpenness);
+        //result &= EyeManager.Instance.GetLeftEyePupilDiameter(out data.LeftEyePupilDiameter);
+        //result &= EyeManager.Instance.GetLeftEyePupilPositionInSensorArea(out data.LeftEyePupilPositionInSensorArea);
+        //result &= EyeManager.Instance.GetRightEyeOrigin(out data.RightEyeOrigin);
+        //result &= EyeManager.Instance.GetRightEyeDirectionNormalized(out data.RightEyeDirectionNormalized);
+        //result &= EyeManager.Instance.GetRightEyeOpenness(out data.RightEyeOpenness);
+        //result &= EyeManager.Instance.GetRightEyePupilDiameter(out data.RightEyePupilDiameter);
+        //result &= EyeManager.Instance.GetRightEyePupilPositionInSensorArea(out data.RightEyePupilPositionInSensorArea);
 
 
-        return result ? data : null;
+
+        // return result ? data : null;
+        return data ;
     }
 
 
